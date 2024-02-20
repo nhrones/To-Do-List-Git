f0b8e9d0212353af589873871c9bce2e37d87629
NDH <ndhrones@gmail.com>
2024-02-20 13:39:59 -0500

    added env .PIN

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 import { initDom } from './dom.js';
 
+let pin = ''
+try {
+pin = process.env.PIN;
+} catch {
+   pin = 'Nope!'
+}
+console.log('pin: ', pin)
+
 // initialize all DOM elements and event handlers 
 await initDom();
